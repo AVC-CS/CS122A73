38ddbf04bce7704df111b632461da0264b54faad
Kyu Lee <kwooler@gmail.com>
2025-10-19 16:46:06 -0700

    A73 init

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ def insertOne(numbers, val):
     Do not use 'sort' keyword
     ########################################
     """
-
+    for i in range(len(numbers)):
+        if numbers[i] >= val:
+            numbers.insert(i, val)
+            return
+    numbers.append(val)
 
 def main():
     numbers = [5, 20, 30, 35, 50]
     print(f'The original list value: {numbers}')
+    print('\n################# Test 1 insert 20 in the middle #######################')
     insertOne(numbers, 25)
     print(f'After insertion 25: {numbers}')
+
+    print('\n################# Test 2 : insert 100 at the end of the list #######################')
+    insertOne(numbers, 100)
+    print(f'After insertion 25: {numbers}')
+
+    print('\n################# Test 3 : insert 0 at the beginning of the list #######################')
+    insertOne(numbers, 0)
+    print(f'After insertion 0: {numbers}')
 
 
 ##
--- main_test.py
+++ main_test.py
@@ -1,30 +1,9 @@
 import main
-import io
-import sys
-import re
-import math
+import pytest
 
-
+@pytest.mark.basic
 def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '25\n'
-    # sys.stdin = io.StringIO(datastr)
-
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
-
+    print (f'\n******************* test_main_1: insert 25 ***********************')
     numbers = [5, 20, 30, 35, 50]
     print(f'The original value {numbers}')
     main.insertOne(numbers, 25)
@@ -32,65 +11,30 @@ def test_main_1():
     assert numbers == [5, 20, 25, 30, 35, 50]
 
 
+@pytest.mark.edge
 def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '0\n'
-    # sys.stdin = io.StringIO(datastr)
-
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
+    print (f'\n******************* test_main_2: insert 100 ***********************')
+    numbers = [5, 20, 30, 35, 50]
+    print(f'The original value {numbers}')
+    main.insertOne(numbers, 100)
+    print(f'After insertion 100 {numbers}')
+    assert numbers == [5, 20, 30, 35, 50, 100]
+    
+@pytest.mark.edge
+def test_main_3():
     numbers = [1, 2, 3, 4, 5]
+    print (f'\n******************* test_main_3: insert 0 ***********************')
     print(f'The original value {numbers}')
     main.insertOne(numbers, 0)
     print(f'After insertion 0 {numbers}')
     assert numbers == [0, 1, 2, 3, 4, 5]
 
-
-def test_main_3():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '0\n'
-    # sys.stdin = io.StringIO(datastr)
-
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
+@pytest.mark.bonus
+def test_main_4():
     numbers = [1, 2, 3, 4, 5]
+    print(f'\n******************* test_main_4: insert 6 ***********************')
     print(f'The original value: {numbers}')
     main.insertOne(numbers, 6)
     print(f'After insertion 6 {numbers}')
-
     assert numbers == [1, 2, 3, 4, 5, 6]
 
-
-def test_main_4():
-    result = False
-    with open('main.py') as f:
-        for line in f:
-            if 'sort(' in line:
-                result = True
-            if 'sorted(' in line:
-                result = True
-    assert result == False
